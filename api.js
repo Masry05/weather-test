d412aa7478b6899f83bbb882a16b47d2d20542fa
Masry05 <138041200+Masry05@users.noreply.github.com>
2023-09-17 00:02:09 +0300

    Add files via upload

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const apiKey = 'e4ada062aaec4d408b1f7d904e363deb';
+/*const apiKey = 'e4ada062aaec4d408b1f7d904e363deb';
 let lat, lon;
 async function getCurrentWeather() {
   if (navigator.geolocation) {
@@ -10,6 +10,33 @@ async function getCurrentWeather() {
         .then((response) => response.json())
         .then((data) => {
           document.querySelector(".temp").innerHTML=`${data.data[0].app_temp}`;
+          document.querySelector(".location").innerHTML=`${data.data[0].timezone}`;
+          
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    });
+  } else {
+    console.error('Geolocation is not supported by this browser.');
+  }
+}*/
+const apiKey = 'a7a038d2d8894643a57204848231609'; // Replace with your WeatherAPI key
+let lat, lon;
+
+async function getCurrentWeather() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`;
+      
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          document.querySelector(".temp").innerHTML = `${data.current.temp_c}°C`;
+          document.querySelector(".location").innerHTML = `${data.location.name}, ${data.location.country}`;
+          console.log(data)
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
@@ -19,4 +46,7 @@ async function getCurrentWeather() {
     console.error('Geolocation is not supported by this browser.');
   }
 }
+
+
+
  
